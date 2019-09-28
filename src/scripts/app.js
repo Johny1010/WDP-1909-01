b47375f64ef29d1948d05d496e8cfe130188de28
Agnieszka Kot <agnieszkakot22@gmail.com>
2019-09-28 22:43:36 +0200

    js to add class on click, style for active class

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,13 @@
 console.log('hello');
+/// WDP190901-5 :add class active on click for buttons
+const addClass = function () {
+    const buttonsOutline = document.querySelectorAll('.btn-outline');
+    buttonsOutline.forEach(button => {
+        button.addEventListener('click', () => {    
+            button.classList.toggle('active');ą
+        });
+    });
+};
+
+addClass();
+
