9796add5ec821fc75ff168cf08c2b6eabb0c752f
gonebada <agnieszkakot22@gmail.com>
2019-10-05 11:50:53 +0200

    Revert "Wdp190901 5"

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,12 +1,1 @@
 console.log('hello');
-/// WDP190901-5 :add class active on click for buttons
-const addClass = function() {
-  const buttonsOutline = document.querySelectorAll('.btn-outline');
-  buttonsOutline.forEach(button => {
-    button.addEventListener('click', () => {
-      button.classList.toggle('active');
-    });
-  });
-};
-
-addClass();
