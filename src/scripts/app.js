4e3b586e576f9eaef5be8884a0def3d78f334add
Anna Pawlik <pawlikanna24@gmail.com>
2019-10-14 14:25:20 +0200

    task-WDP190901-18

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,29 @@
-console.log('hello');
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line no-undef
+const slider = tns({
+  container: '.my-slider',
+  items: 4,
+  controls: false,
+  loop: true,
+  mouseDrag: true,
+  touch: true,
+  autoHeight: true,
+  speed: 400,
+  responsive: {
+    320: {
+      edgePadding: 20,
+      gutter: 20,
+      items: 1
+    },
+    768: {
+      gutter: 30,
+      items: 2
+    },
+    992: {
+      items: 3
+    },
+    1200: {
+      items: 4
+    }
+  }
+});
